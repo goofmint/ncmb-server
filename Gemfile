d8716dc7152c048dbc3615c307deb390a436dfeb
Atsushi Nakatsugawa <atsushi@moongift.jp>
2014-08-20 13:28:56 +0900

    Init

--- Gemfile
+++ Gemfile
@@ -3,4 +3,3 @@ ruby '2.0.0'
 
 gem 'sinatra'
 gem 'sinatra-reloader'
-
--- app.rb
+++ app.rb
@@ -7,8 +7,9 @@ require 'json'
 set :root, File.dirname(__FILE__)
 set :public_folder, Proc.new { File.join(root, "public") }
 post '/sign' do
+  response.headers['Access-Control-Allow-Origin'] = '*'
   content_type :json
-  CLIENT_KEY = 'your_client_key'
+  CLIENT_KEY = '65c3eb97f9fe9e90bba279611d405121ddc1ea9beaea92ea77ee7ceb02b91247'
   json = JSON.parse(request.body.read)
   {signature: Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha256'), CLIENT_KEY, json["forEncodeString"])).strip()}.to_json
 end
